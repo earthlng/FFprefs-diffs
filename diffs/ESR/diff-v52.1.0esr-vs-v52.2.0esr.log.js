0a42cbdff42bd7a815aed851620db7c62929aff2
earthlng <earthlng@users.noreply.github.com>
2017-06-16 17:34:01 +0200

    Create diff-v52.1.0esr-vs-v52.2.0esr.log.js

---
+++ diffs/ESR/diff-v52.1.0esr-vs-v52.2.0esr.log.js
@@ -0,0 +1,11 @@
+/******************************************************************************************
+ * diffs between FF prefs v52.1.0esr and v52.2.0esr
+ *
+ * prefs: 2799
+ *        2796 matching prefs
+ *           3 diffs ( 3 new, 0 gone, 0 different )
+ ******************************************************************************************/
+// new in v52.2.0esr:
+pref("intl.tsf.associate_imc_only_when_imm_ime_is_active", false);
+pref("intl.tsf.hack.ms_japanese_ime.do_not_associate_imc_on_win10", true);
+pref("signon.masterPasswordReprompt.timeout_ms", 900000);
