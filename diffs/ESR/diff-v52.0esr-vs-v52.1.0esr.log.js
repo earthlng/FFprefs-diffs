5ff6a7889e0587c524b8fa4b1e0727fa5730ad65
earthlng <earthlng@users.noreply.github.com>
2017-06-16 17:36:33 +0200

    Create diff-v52.0esr-vs-v52.1.0esr.log.js

---
+++ diffs/ESR/diff-v52.0esr-vs-v52.1.0esr.log.js
@@ -0,0 +1,10 @@
+/******************************************************************************************
+ * diffs between FF prefs v52.0esr and v52.1.0esr
+ *
+ * prefs: 2796
+ *        2794 matching prefs
+ *           2 diffs ( 0 new, 0 gone, 2 different )
+ ******************************************************************************************/
+// changed in v52.1.0esr:
+pref("network.predictor.enable-prefetch", false); // prev: true
+pref("security.pki.certificate_transparency.mode", 0); // prev: 1
