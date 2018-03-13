f62a2efc5f1569bbc54ca42c2fb26d3e91e04c50
earthlng <earthlng@users.noreply.github.com>
2018-03-13 14:35:51 +0100

    Create diff-v52.6.0esr-vs-v52.7.0esr.log.js

---
+++ diffs/ESR/diff-v52.6.0esr-vs-v52.7.0esr.log.js
@@ -0,0 +1,15 @@
+/******************************************************************************************
+ * diffs between FF prefs v52.6.0esr and v52.7.0esr
+ *
+ * prefs: 2799
+ *        2797 matching prefs
+ *           2 diffs ( 0 new, 1 gone, 1 different )
+ ******************************************************************************************/
+// new in v52.7.0esr:
+
+// removed, renamed or hidden in v52.7.0esr:
+pref("browser.search.order.US.3", "data:text/plain,browser.search.order.US.3=Bing");
+
+// changed in v52.7.0esr:
+pref("browser.search.order.US.2", "data:text/plain,browser.search.order.US.2=Bing"); // prev: "data:text/plain,browser.search.order.US.2=Yahoo"
+
