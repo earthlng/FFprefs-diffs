a3cc01cfe83b0ce117a6b9b1cdce48c0198d234e
earthlng <earthlng@users.noreply.github.com>
2018-08-26 17:26:36 +0000

    Create diff-v60.0esr-vs-v60.1.0esr.log.js

---
+++ diffs/ESR/diff-v60.0esr-vs-v60.1.0esr.log.js
@@ -0,0 +1,15 @@
+/******************************************************************************************
+ * diffs between FF prefs v60.0esr and v60.1.0esr
+ *
+ * prefs: 3150
+ *        3147 matching prefs
+ *           3 diffs ( 0 new, 1 gone, 2 different )
+ ******************************************************************************************/
+// new in v60.1.0esr:
+
+// removed, renamed or hidden in v60.1.0esr:
+pref("dom.ipc.scheduler", false);
+
+// changed in v60.1.0esr:
+pref("extensions.langpacks.signatures.required", true); // prev: false
+pref("media.wmf.amd.vp9.enabled", false); // prev: true
