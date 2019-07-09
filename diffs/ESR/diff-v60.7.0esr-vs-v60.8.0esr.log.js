a95cbab864b372be3709465b097e98e03d1849e2
earthlng <earthlng@users.noreply.github.com>
2019-07-09 09:48:54 +0000

    Create diff-v60.7.0esr-vs-v60.8.0esr.log.js

---
+++ diffs/ESR/diff-v60.7.0esr-vs-v60.8.0esr.log.js
@@ -0,0 +1,23 @@
+/******************************************************************************************
+ * diffs between FF prefs v60.7.0esr and v60.8.0esr
+ *
+ * prefs: 3151
+ *        3138 matching prefs
+ *          13 diffs ( 1 new, 12 gone, 0 different )
+ ******************************************************************************************/
+// new in v60.8.0esr:
+pref("services.sync.prefs.dangerously_allow_arbitrary", false);
+
+// removed, renamed or hidden in v60.8.0esr:
+pref("services.sync.prefs.sync.browser.safebrowsing.downloads.enabled", true);
+pref("services.sync.prefs.sync.browser.safebrowsing.malware.enabled", true);
+pref("services.sync.prefs.sync.browser.safebrowsing.passwords.enabled", true);
+pref("services.sync.prefs.sync.browser.safebrowsing.phishing.enabled", true);
+pref("services.sync.prefs.sync.extensions.personas.current", true);
+pref("services.sync.prefs.sync.pref.advanced.images.disable_button.view_image", true);
+pref("services.sync.prefs.sync.pref.advanced.javascript.disable_button.advanced", true);
+pref("services.sync.prefs.sync.security.OCSP.enabled", true);
+pref("services.sync.prefs.sync.security.OCSP.require", true);
+pref("services.sync.prefs.sync.security.tls.version.max", true);
+pref("services.sync.prefs.sync.security.tls.version.min", true);
+pref("services.sync.prefs.sync.xpinstall.whitelist.required", true);
