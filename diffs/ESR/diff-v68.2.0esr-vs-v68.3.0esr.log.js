9cbdf3e64242f4d7c6cd44fdec400848a30f8f2b
earthlng <earthlng@users.noreply.github.com>
2019-12-04 12:52:06 +0000

    Create diff-v68.2.0esr-vs-v68.3.0esr.log.js

---
+++ diffs/ESR/diff-v68.2.0esr-vs-v68.3.0esr.log.js
@@ -0,0 +1,14 @@
+/******************************************************************************************
+ * diffs between FF prefs v68.2.0esr and v68.3.0esr
+ *
+ * prefs: 3493
+ *        3491 matching prefs
+ *           2 diffs ( 0 new, 0 gone, 2 different )
+ ******************************************************************************************/
+// new in v68.3.0esr:
+
+// removed, renamed or hidden in v68.3.0esr:
+
+// changed in v68.3.0esr:
+pref("app.update.staging.enabled", false); // prev: true
+pref("security.ssl.errorReporting.enabled", false); // prev: true
