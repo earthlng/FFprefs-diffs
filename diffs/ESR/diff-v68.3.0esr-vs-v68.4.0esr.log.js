a26a9528a2924ac3c31e65f9aa56065c67b92d30
earthlng <earthlng@users.noreply.github.com>
2020-01-07 17:26:18 +0000

    Create diff-v68.3.0esr-vs-v68.4.0esr.log.js

---
+++ diffs/ESR/diff-v68.3.0esr-vs-v68.4.0esr.log.js
@@ -0,0 +1,14 @@
+/******************************************************************************************
+ * diffs between FF prefs v68.3.0esr and v68.4.0esr
+ *
+ * prefs: 3494
+ *        3492 matching prefs
+ *           2 diffs ( 1 new, 0 gone, 1 different )
+ ******************************************************************************************/
+// new in v68.4.0esr:
+pref("layout.css.moz-binding.content.enabled", false);
+
+// removed, renamed or hidden in v68.4.0esr:
+
+// changed in v68.4.0esr:
+pref("app.update.staging.enabled", true); // prev: false
