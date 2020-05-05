d77a333c87b6e5e3a446c5ed762597fed674b943
earthlng <earthlng@users.noreply.github.com>
2020-05-05 12:06:07 +0000

    Create diff-v68.7.0esr-vs-v68.8.0esr.log.js

---
+++ diffs/ESR/diff-v68.7.0esr-vs-v68.8.0esr.log.js
@@ -0,0 +1,13 @@
+/******************************************************************************************
+ * diffs between FF prefs v68.7.0esr and v68.8.0esr
+ *
+ * prefs: 3494
+ *        3493 matching prefs
+ *           1 diffs ( 0 new, 0 gone, 1 different )
+ ******************************************************************************************/
+// new in v68.8.0esr:
+
+// removed, renamed or hidden in v68.8.0esr:
+
+// changed in v68.8.0esr:
+pref("security.sandbox.content.level", 6); // prev: 5
