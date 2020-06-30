122e0a2fc2360a6bab6371fdc4f70c9018c9ecad
earthlng <earthlng@users.noreply.github.com>
2020-06-30 12:41:57 +0000

    Create diff-v78.0-vs-v78.0esr.log.js

---
+++ diffs/7x/diff-v78.0-vs-v78.0esr.log.js
@@ -0,0 +1,13 @@
+/******************************************************************************************
+ * diffs between FF prefs v78.0 and v78.0esr
+ *
+ * prefs: 4042
+ *        4041 matching prefs
+ *           1 diffs ( 0 new, 0 gone, 1 different )
+ ******************************************************************************************/
+// new in v78.0esr:
+
+// removed, renamed or hidden in v78.0esr:
+
+// changed in v78.0esr:
+pref("app.update.channel", "esr"); // prev: "release"
