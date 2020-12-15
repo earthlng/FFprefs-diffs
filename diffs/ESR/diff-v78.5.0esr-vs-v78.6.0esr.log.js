b1d40653c3438682065e7dcc316a73e89ea22815
earthlng <earthlng@users.noreply.github.com>
2020-12-15 14:37:21 +0000

    Create diff-v78.5.0esr-vs-v78.6.0esr.log.js

---
+++ diffs/ESR/diff-v78.5.0esr-vs-v78.6.0esr.log.js
@@ -0,0 +1,13 @@
+/******************************************************************************************
+ * diffs between FF prefs v78.5.0esr and v78.6.0esr
+ *
+ * prefs: 4045
+ *        4044 matching prefs
+ *           1 diffs ( 0 new, 0 gone, 1 different )
+ ******************************************************************************************/
+// new in v78.6.0esr:
+
+// removed, renamed or hidden in v78.6.0esr:
+
+// changed in v78.6.0esr:
+pref("network.dns.dns_query_single_label", false); // prev: true
