0daf75df2021449b7197b42cf197d5253826ced2
earthlng <earthlng@users.noreply.github.com>
2021-04-21 13:51:19 +0000

    Create diff-v78.9.0esr-vs-v78.10.0esr.log.js

---
+++ diffs/ESR/diff-v78.9.0esr-vs-v78.10.0esr.log.js
@@ -0,0 +1,13 @@
+/******************************************************************************************
+ * diffs between FF prefs v78.9.0esr and v78.10.0esr
+ *
+ * prefs: 4047
+ *        4046 matching prefs
+ *           1 diffs ( 1 new, 0 gone, 0 different )
+ ******************************************************************************************/
+// new in v78.10.0esr:
+pref("svg.context-properties.content.allowed-domains", "profile.accounts.firefox.com,profile.stage.mozaws.net");
+
+// removed, renamed or hidden in v78.10.0esr:
+
+// changed in v78.10.0esr:
