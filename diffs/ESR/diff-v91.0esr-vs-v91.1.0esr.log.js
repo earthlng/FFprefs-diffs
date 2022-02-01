3fe3b47c409c4f42c4bf584681e75f057ae20cfc
earthlng <earthlng@users.noreply.github.com>
2022-02-01 18:32:29 +0000

    Create diff-v91.0esr-vs-v91.1.0esr.log.js

---
+++ diffs/ESR/diff-v91.0esr-vs-v91.1.0esr.log.js
@@ -0,0 +1,19 @@
+/******************************************************************************************
+ * diffs between FF prefs v91.0esr and v91.1.0esr
+ *
+ * prefs: 4325
+ *        4318 matching prefs
+ *           7 diffs ( 7 new, 0 gone, 0 different )
+ ******************************************************************************************/
+// new in v91.1.0esr:
+pref("general.autoscroll.prevent_to_start.altKey", false);
+pref("general.autoscroll.prevent_to_start.ctrlKey", false);
+pref("general.autoscroll.prevent_to_start.metaKey", false);
+pref("general.autoscroll.prevent_to_start.osKey", false);
+pref("general.autoscroll.prevent_to_start.shiftKey", true);
+pref("javascript.options.use_fdlibm_for_sin_cos_tan", false);
+pref("network.protocol-handler.external.mk", false);
+
+// removed, renamed or hidden in v91.1.0esr:
+
+// changed in v91.1.0esr:
