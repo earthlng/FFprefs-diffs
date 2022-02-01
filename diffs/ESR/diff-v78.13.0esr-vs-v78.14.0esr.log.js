3e10fc2ea02066c15c02a807ef631bf83ec2ce15
earthlng <earthlng@users.noreply.github.com>
2022-02-01 18:29:43 +0000

    Create diff-v78.13.0esr-vs-v78.14.0esr.log.js

---
+++ diffs/ESR/diff-v78.13.0esr-vs-v78.14.0esr.log.js
@@ -0,0 +1,13 @@
+/******************************************************************************************
+ * diffs between FF prefs v78.13.0esr and v78.14.0esr
+ *
+ * prefs: 4049
+ *        4048 matching prefs
+ *           1 diffs ( 1 new, 0 gone, 0 different )
+ ******************************************************************************************/
+// new in v78.14.0esr:
+pref("network.protocol-handler.external.mk", false);
+
+// removed, renamed or hidden in v78.14.0esr:
+
+// changed in v78.14.0esr:
