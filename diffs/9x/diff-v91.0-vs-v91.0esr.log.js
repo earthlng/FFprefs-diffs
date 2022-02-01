88d491daf51dfe1dffe1f9a845cb05529acc2e25
earthlng <earthlng@users.noreply.github.com>
2022-02-01 18:46:56 +0000

    Create diff-v91.0-vs-v91.0esr.log.js

---
+++ diffs/9x/diff-v91.0-vs-v91.0esr.log.js
@@ -0,0 +1,13 @@
+/******************************************************************************************
+ * diffs between FF prefs v91.0 and v91.0esr
+ *
+ * prefs: 4318
+ *        4317 matching prefs
+ *           1 diffs ( 0 new, 0 gone, 1 different )
+ ******************************************************************************************/
+// new in v91.0esr:
+
+// removed, renamed or hidden in v91.0esr:
+
+// changed in v91.0esr:
+pref("app.update.channel", "esr"); // prev: "release"
