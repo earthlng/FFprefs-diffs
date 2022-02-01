1360d65c145235fb2356825b7a6b2bc32e98c5d7
earthlng <earthlng@users.noreply.github.com>
2022-02-01 18:29:08 +0000

    Create diff-v78.12.0esr-vs-v78.13.0esr.log.js

---
+++ diffs/ESR/diff-v78.12.0esr-vs-v78.13.0esr.log.js
@@ -0,0 +1,13 @@
+/******************************************************************************************
+ * diffs between FF prefs v78.12.0esr and v78.13.0esr
+ *
+ * prefs: 4048
+ *        4047 matching prefs
+ *           1 diffs ( 1 new, 0 gone, 0 different )
+ ******************************************************************************************/
+// new in v78.13.0esr:
+pref("network.dns.copy_string_before_call", true);
+
+// removed, renamed or hidden in v78.13.0esr:
+
+// changed in v78.13.0esr:
