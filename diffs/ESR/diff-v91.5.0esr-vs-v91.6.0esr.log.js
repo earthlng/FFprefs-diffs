ee15009f3692857ae0fa679b6bfeb6b5c3e5aa23
earthlng <earthlng@users.noreply.github.com>
2022-02-09 18:39:28 +0000

    Create diff-v91.5.0esr-vs-v91.6.0esr.log.js

---
+++ diffs/ESR/diff-v91.5.0esr-vs-v91.6.0esr.log.js
@@ -0,0 +1,15 @@
+/******************************************************************************************
+ * diffs between FF prefs v91.5.0esr and v91.6.0esr
+ *
+ * prefs: 4327
+ *        4324 matching prefs
+ *           3 diffs ( 0 new, 0 gone, 3 different )
+ ******************************************************************************************/
+// new in v91.6.0esr:
+
+// removed, renamed or hidden in v91.6.0esr:
+
+// changed in v91.6.0esr:
+pref("app.update.url.details", "https://www.mozilla.org/%LOCALE%/firefox/organizations/notes"); // prev: "https://www.mozilla.org/%LOCALE%/firefox/notes"
+pref("app.update.url.manual", "https://www.mozilla.org/%LOCALE%/firefox/enterprise?reason=manual-update"); // prev: "https://www.mozilla.org/%LOCALE%/firefox/"
+pref("dom.vr.enabled", false); // prev: true
