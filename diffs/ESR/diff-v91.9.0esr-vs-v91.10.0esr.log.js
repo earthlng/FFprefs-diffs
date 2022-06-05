81b2190411fc49216ce397e0b0df61140a0be30b
earthlng <earthlng@users.noreply.github.com>
2022-06-05 11:37:33 +0000

    Create diff-v91.9.0esr-vs-v91.10.0esr.log.js

---
+++ diffs/ESR/diff-v91.9.0esr-vs-v91.10.0esr.log.js
@@ -0,0 +1,14 @@
+/******************************************************************************************
+ * diffs between FF prefs v91.9.0esr and v91.10.0esr
+ *
+ * prefs: 4328
+ *        4326 matching prefs
+ *           2 diffs ( 1 new, 0 gone, 1 different )
+ ******************************************************************************************/
+// new in v91.10.0esr:
+pref("media.gmp-manager.checkContentSignature", true);
+
+// removed, renamed or hidden in v91.10.0esr:
+
+// changed in v91.10.0esr:
+pref("network.http.spdy.websockets", false); // prev: true
